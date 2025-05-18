3d5130121ceeea0c810b8df197dd4f7a0f9aa6b1
sourish-007 <sourishsarkar0011@gmail.com>
2025-05-18 13:43:58 +0530

    routes fixed

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://bloggify-1-qx7w.onrender.com",
   credentials: true,
 }));
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: 'https://bloggify-z8j2.onrender.com'
 });
 
 axiosInstance.interceptors.request.use(config => {
